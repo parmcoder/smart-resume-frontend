9f67f074fc34b8cda2425708e6a03e891e4c56ba
Parmcoder <possawat2017@hotmail.com>
2022-09-10 18:58:37 +0700

    feat: added playground page

---
+++ src/components/banners/Banner.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+    <div class="banner">
+        {{ msg }}
+    </div>
+</template>
+
+<style lang="scss">
+.banner {
+    color: white;
+    min-width: 500px;
+    min-height: 200px;
+}
+</style>
---
+++ src/views/Playground.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Banner from '../components/banners/Banner.vue';
+</script>
+
+<template>
+    <Banner msg="Parm's Blog"></Banner>
+</template>
