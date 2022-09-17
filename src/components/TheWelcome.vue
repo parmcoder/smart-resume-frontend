146fae7ab394dfbe4aacf3875534a55db2071765
Parmcoder <possawat2017@hotmail.com>
2022-09-17 23:02:32 +0700

    fix: clean up missing files

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
   <WelcomeItem>
     <template #icon>
-      <DocumentationIcon />
     </template>
     <template #heading>Documentation</template>
 
@@ -21,7 +15,6 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
     </template>
     <template #heading>Tooling</template>
 
@@ -41,7 +34,6 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <EcosystemIcon />
     </template>
     <template #heading>Ecosystem</template>
 
@@ -57,7 +49,6 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <CommunityIcon />
     </template>
     <template #heading>Community</template>
 
@@ -75,7 +66,6 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <SupportIcon />
     </template>
     <template #heading>Support Vue</template>
 
